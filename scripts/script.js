165e7e9f62b3ec81d5976b6294a71b012f79bc88
Gustavo Jordao <gustavo.jordao021@gmail.com>
2019-12-05 16:50:21 -0500

    Start landing, instructions page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 window.onload = function() {
     document.getElementById("btn").onclick = function() {
+        let introDiv = document.getElementById("intro-page");
+        introDiv.innerHTML = "";
+        introDiv.innerHTML = 
+        '<div><canvas id="canvas" width="1024" height="576"></canvas><button id="btn">START GAME</button>   </div>';
         startGame();
     };
   
